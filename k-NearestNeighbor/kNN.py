c6fe9aae2f67d4a4d71fc561885a6bd276f7499c
yaoxs1996 <infinity_yao@hotmail.com>
2019-08-22 17:19:56 +0800

    kNN

--- k-NearestNeighbor/kNN.py
+++ k-NearestNeighbor/kNN.py
@@ -8,6 +8,7 @@ from os import listdir
 '''
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]      #shape是查看矩阵或数组的维数
+    #距离计算
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2        #**为幂
     sqDistances = sqDiffMat.sum(axis=1)
@@ -17,7 +18,8 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    #注意，python3不再支持dict的iteritems方法，改为item方法
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 # 创建数据集和标签
@@ -26,6 +28,8 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+#将文本记录转换成NumPy
+#测试文本使用2号
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())     #获得文件行数
@@ -34,13 +38,14 @@ def file2matrix(filename):
     fr = open(filename)
     index = 0
     for line in fr.readlines():
-        line = line.strip()     #移除字符串首尾的指定字符，默认空格
+        line = line.strip()     #截取掉所有回车
         listFromLine = line.split('\t')
         returnMat[index, :] = listFromLine[0:3]
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat, classLabelVector
 
+#归一化特征值
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
