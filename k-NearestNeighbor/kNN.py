c7b792676bb08b50a85b17af9619a01b8352ea8f
yaoxs1996 <infinity_yao@hotmail.com>
2019-08-22 16:31:29 +0800

    kNN

--- k-NearestNeighbor/kNN.py
+++ k-NearestNeighbor/kNN.py
@@ -1,11 +1,11 @@
-'''
-
-'''
-
 from numpy import *
 import operator
 from os import listdir
 
+'''
+使用k-近邻算法将每组数据划分到某个类中
+分类的输入向量inX、训练集样本dataSet、标签向量labels、k表示选择最近邻居的数目
+'''
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]      #shape是查看矩阵或数组的维数
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
@@ -20,6 +20,7 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+# 创建数据集和标签
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
@@ -42,4 +43,60 @@ def file2matrix(filename):
 
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
-    
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.50
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')     #从文件中载入数据集
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" %(classifierResult, datingLabels[i]))
+        if(classifierResult != datingLabels[i]):
+            errorCount += 1.0
+        print("the total error rate is: %f" %(errorCount/float(numTestVecs)))
+        print(errorCount)
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readlines()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNameStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNameStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' %fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' %fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" %(classifierResult, classNumStr))
+        if(classifierResult != classNumStr):
+            errorCount += 1.0
+        print("\nthe total number of errors is: %d" %errorCount)
+        print("\nthe total error rate is: %f" %(errorCount/float(mTest)))
