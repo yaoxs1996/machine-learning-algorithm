5af8044a05176d31fb877724c78508bf53e3f6a5
yxs <infinity_yao@hotmail.com>
2019-08-29 20:16:20 +0800

    calculate Shannon Entropie

---
+++ DecisionTree/tree.py
@@ -0,0 +1,29 @@
+from math import log
+
+# 计算给定数据集的熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+
+    # 为所有可能的分类创建字典
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)       # 求以2为底的对数
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+            [1, 1, 'yes'],
+            [1, 0, 'no'],
+            [0, 1, 'no'],
+            [0, 1, 'no'],
+            [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
