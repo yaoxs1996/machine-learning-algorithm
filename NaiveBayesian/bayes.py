42bd6c97f3581dca850b719212814c9694ada3d1
yaoxs1996 <infinity_yao@hotmail.com>
2019-09-07 11:01:21 +0800

    naive bayes

--- NaiveBayesian/bayes.py
+++ NaiveBayesian/bayes.py
@@ -1,3 +1,6 @@
+from numpy import *
+import math
+
 # 词表到向量的转换函数
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -24,3 +27,61 @@ def setOfWord2Vec(vocabList, inputSet):
             print("the word: %s is not in my Vocabulary!" %word)
     return returnVec
     
+# 朴素贝叶斯分类器训练函数
+# 文档矩阵trainMatrix，文档类别标签向量trainCategory
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # 初始化概率
+    '''
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    '''
+    # 降低概率为0的影响，将词的出现次数初始化为1，分母初始化为2
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            # 向量相加
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 对每个元素做除法
+    '''
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+    '''
+    # 取log，防止下溢
+    p1Vect = math.log(p1Num / p1Denom)
+    p0Vect = math.log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+# 朴素贝叶斯分类函数
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + math.log(pClass1)      # 元素相乘
+    p0 = sum(vec2Classify * p0Vec) + math.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWord2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
+    print(testEntry, ' classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
+    print(testEntry, ' classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
--- NaiveBayesian/test.py
+++ NaiveBayesian/test.py
@@ -2,6 +2,21 @@ import bayes
 
 listOPosts, listClasses = bayes.loadDataSet()
 myVocabList = bayes.createVocabList(listOPosts)
+'''
 print(myVocabList)
 print(bayes.setOfWord2Vec(myVocabList, listOPosts[0]))
 print(bayes.setOfWord2Vec(myVocabList, listOPosts[3]))
+'''
+
+# 朴素贝叶斯分类器
+'''
+trainMat = []
+for postinDoc in listOPosts:
+    trainMat.append(bayes.setOfWord2Vec(myVocabList, postinDoc))
+p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+print(pAb)
+print(p0V)
+print(p1V)
+'''
+
+bayes.testingNB()
